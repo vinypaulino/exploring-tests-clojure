d3f1f4db8253e852dbdb54cafd3e4fbd7c8ee1f0
Vinicius Paulino <vinicius.paulino@vinicius.paulino>
2024-01-22 10:37:03 -0300

    feat: adds function with when-let function

--- src/exploring_tests/logic.clj
+++ src/exploring_tests/logic.clj
@@ -9,6 +9,14 @@
 
 ;adding when-let to get department and when it not passed that function return false
 
+;working even when department is nil using when-let function
+;(defn fits-in-queue
+;  [hospital department]
+;  (when-let [queue (get hospital department)]
+;    (-> queue
+;        count
+;        (< 5))))
+
 (defn fits-in-queue?
   [hospital department]
   (some-> hospital
